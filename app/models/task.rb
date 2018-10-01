5eb600649c33a8e4709ea968f3870ad7ebf53650
ibrahim-saifee <ibrahim.saifee786@gmail.com>
2018-10-01 18:12:41 +0530

    Added association Task belongs_to TaskType.

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,10 @@ class Task < ApplicationRecord
   enum  status: [:pending, :in_progress, :completed]
 
   validates :title, :position, :status, presence: true
+
+  belongs_to :task_type
+
+  def type
+    task_type.name
+  end
 end
--- app/models/task_type.rb
+++ app/models/task_type.rb
@@ -1,3 +1,5 @@
 class TaskType < ApplicationRecord
   validates :name, presence: true
+
+  has_many :tasks
 end
